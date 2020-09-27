3d8648740223d535e4b93376885a07c6f08f42d2
rycont <reactdev@kakao.com>
2020-09-27 19:41:05 +0900

    chore : 문과만세

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{
+        color: "black",
+        fontSize: 40,
+        transform: [{
+          scaleX: 0.9
+        }]
+      }}>문과 만세</Text>
+      <Image source={{
+        uri: 'https://mblogthumb-phinf.pstatic.net/20150504_21/aikawayui_1430678048359rjhl7_JPEG/%C1%A6%BA%ED%C1%F8%B0%DD%C0%C7_%B0%C5%C0%CE_0903-33-50.JPG?type=w2',
+        width: 500,
+        height: 300
+      }} />
     </View>
   );
 }
@@ -14,7 +24,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
